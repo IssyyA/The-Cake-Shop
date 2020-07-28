21f7daf26641be430671e0690471c8bc56628eea
IssyyA <isiomaandrew@gmail.com>
2020-07-28 16:14:05 +0100

    Updates

---
+++ precache-manifest.d75effd0e4c2efb593fe7e9a99f170e1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303694330ec4e43172df8ddd8d053dc8",
+    "url": "/The-Cake-Shop/index.html"
+  },
+  {
+    "revision": "62504fdd66a3e44d36f1",
+    "url": "/The-Cake-Shop/static/css/2.ea69d5dd.chunk.css"
+  },
+  {
+    "revision": "d078076d5f968be3cca7",
+    "url": "/The-Cake-Shop/static/css/main.4a4b386e.chunk.css"
+  },
+  {
+    "revision": "62504fdd66a3e44d36f1",
+    "url": "/The-Cake-Shop/static/js/2.c79ad97d.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/The-Cake-Shop/static/js/2.c79ad97d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d078076d5f968be3cca7",
+    "url": "/The-Cake-Shop/static/js/main.7a0ca81c.chunk.js"
+  },
+  {
+    "revision": "c2eb2aea98f4c18f3104",
+    "url": "/The-Cake-Shop/static/js/runtime-main.b96149b2.js"
+  },
+  {
+    "revision": "0ccc21ab2297c6c9e665b686860830fd",
+    "url": "/The-Cake-Shop/static/media/Ellipse 11.0ccc21ab.png"
+  },
+  {
+    "revision": "470500e0b9a02a57efe6e59a109d6041",
+    "url": "/The-Cake-Shop/static/media/Ellipse 14.470500e0.png"
+  },
+  {
+    "revision": "151718752b28f05d305810cf566c669f",
+    "url": "/The-Cake-Shop/static/media/Ellipse 8.15171875.png"
+  },
+  {
+    "revision": "b21af39c59099f496fca877db10ed350",
+    "url": "/The-Cake-Shop/static/media/Rectangle 20.b21af39c.png"
+  },
+  {
+    "revision": "db690e9c9028e53a49c05aba83f31511",
+    "url": "/The-Cake-Shop/static/media/Rectangle 29.db690e9c.png"
+  },
+  {
+    "revision": "dd4c90cd78110f7f1ea5e869022cdec4",
+    "url": "/The-Cake-Shop/static/media/Rectangle 30.dd4c90cd.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/The-Cake-Shop/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/The-Cake-Shop/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/The-Cake-Shop/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/The-Cake-Shop/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/The-Cake-Shop/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "35ab343045faef27a173ea86b83d956e",
+    "url": "/The-Cake-Shop/static/media/image 1.35ab3430.png"
+  },
+  {
+    "revision": "facc173911a3688f637f64cef6eb6661",
+    "url": "/The-Cake-Shop/static/media/logo.facc1739.png"
+  }
+]);
